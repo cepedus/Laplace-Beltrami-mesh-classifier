02959820d1c8e5f0f0434ed18c717a89799ab340
Timothée Darcet <timothee.darcet+git@m4x.org>
2019-12-08 15:18:49 +0100

    Commit build directory

---
+++ build/CMakeFiles/project_bin.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/timothee/Documents/Docs_X/INF/INF574/Projet_ShapeRetrieval/main.cpp" "/home/timothee/Documents/Docs_X/INF/INF574/Projet_ShapeRetrieval/build/CMakeFiles/project_bin.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/timothee/Documents/Docs_X/INF/INF574/libigl/cmake/../include"
+  "/home/timothee/Documents/Docs_X/INF/INF574/libigl/cmake/../external/eigen"
+  "/home/timothee/Documents/Docs_X/INF/INF574/libigl/external/glad/include"
+  "/home/timothee/Documents/Docs_X/INF/INF574/libigl/external/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/timothee/Documents/Docs_X/INF/INF574/Projet_ShapeRetrieval/build/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/home/timothee/Documents/Docs_X/INF/INF574/Projet_ShapeRetrieval/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/project_bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_bin.dir/main.cpp.o"
+  "project_bin.pdb"
+  "project_bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
