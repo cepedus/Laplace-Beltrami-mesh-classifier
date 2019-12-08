d8a62a975ca7ba1fba08f5b0f751faf186d69fc6
Timothée Darcet <timothee.darcet+git@m4x.org>
2019-12-08 14:53:37 +0100

    Work on driver

--- driver.py
+++ driver.py
@@ -14,9 +14,13 @@ with open(nodes_FN, 'r') as nodes_F:
             if model.split('.')[-1] == "obj":
                 # print(model)
                 # for node in nodes_F.readlines():
-                print("Doing ", model)
-                process = subprocess.Popen([cmd, model[:-4]], stdout=subprocess.PIPE)
-                output, error = process.communicate()
-                print(output)
-                print(error, file=sys.stderr)
+                node = nodes_F.readline()
+                if node == "":
+                    nodes_F.seek(0)
+                    node = nodes_F.readline()
+                print("Doing {} on {}".format(model, node))
+                # process = subprocess.Popen([cmd, model[:-4]], stdout=subprocess.PIPE)
+                # output, error = process.communicate()
+                # print(output)
+                # print(error, file=sys.stderr)
 
