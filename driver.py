365808c1cdb17b47574dccc88fae280d8a2a6263
Timothée Darcet <timothee.darcet+git@m4x.org>
2019-12-08 16:30:42 +0100

    Better reading of node file

--- driver.py
+++ driver.py
@@ -16,10 +16,14 @@ with open(nodes_FN, 'r') as nodes_F:
             if model.split('.')[-1] == "obj":
                 # print(model)
                 # for node in nodes_F.readlines():
-                node = nodes_F.readline()
-                if node == "":
-                    nodes_F.seek(0)
+                node = ""
+                while node == "":
                     node = nodes_F.readline()
+                    while node == "\n":
+                        node = nodes_F.readline()
+                    if node == "":
+                        nodes_F.seek(0)
+                node = node.strip()
                 print("Doing {} on {}".format(model, node))
                 process = subprocess.Popen(["ssh", "-oStrictHostKeyChecking=no", username + "@" + node, "\"tmux new-session -s", "project_embeddings", "-d \\\"../project_bin", model, "\\\"\""], stdout=subprocess.PIPE)
                 output, error = process.communicate()
